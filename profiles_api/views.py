e73e2383afe7bb0f1895f606ced93f3a6d13e865
Chris Lattman <natgeo13@gmail.com>
2020-09-04 01:22:05 -0400

    Converted indentation to spaces

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,42 +6,42 @@ from profiles_api import serializers
 
 
 class HelloApiView(APIView):
-	"""Test API View"""
-	serializer_class = serializers.HelloSerializer
+    """Test API View"""
+    serializer_class = serializers.HelloSerializer
 
-	def get(self, request, format=None):
-		"""Returns a list of APIView features"""
-		an_apiview = [
-			'Uses HTTP methods as function (get, post, patch, put, delete)',
-			'Is similar to a traditional Django View',
-			'Gives you the most control over your application logic',
-			'Is mapped manually to URLs',
-		]
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your application logic',
+            'Is mapped manually to URLs',
+        ]
 
-		return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
 
-	def post(self, request):
-		"""Create a hello message with our name"""
-		serializer = self.serializer_class(data=request.data)
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
 
-		if serializer.is_valid():
-			name = serializer.validated_data.get('name')
-			message = f'Hello {name}'
-			return Response({'message': message})
-		else:
-			return Response(
-				serializer.errors, 
-				status=status.HTTP_400_BAD_REQUEST
-			)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors, 
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
-	def put(self, request, pk=None):
-		"""Handle updating an object"""
-		return Response({'method': 'PUT'})
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({'method': 'PUT'})
 
-	def patch(self, request, pk=None):
-		"""Handle a partial update of an object"""
-		return Response({'method': 'PATCH'})
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({'method': 'PATCH'})
 
-	def delete(self, request, pk=None):
-		"""Delete an object"""
-		return Response({'method': 'DELETE'})
+    def delete(self, request, pk=None):
+        """Delete an object"""
+        return Response({'method': 'DELETE'})
